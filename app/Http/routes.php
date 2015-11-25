0ffa40dcffdb73ed9ea71e49777b8a63709e8057
Salahuddin Hairai <mr.od3n@gmail.com>
2015-11-25 16:14:35 +0800

    Add Ticket routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,3 +37,6 @@ Route::post('password/email', 'Auth\PasswordController@postEmail');
 // Password reset routes...
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+// Ticket routes
+Route::resource('tickets', 'TicketsController');
