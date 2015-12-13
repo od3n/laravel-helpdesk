ff9c72f3866a8ed5f4e70aee7b1b638728cc7756
Salahuddin Hairai <mr.od3n@gmail.com>
2015-12-13 11:19:03 +0800

    Update home template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('master')        
+@extends('master')
 @section('title', 'Home')
 
 @section('content')
@@ -8,38 +8,9 @@
             @include('shared.notification')
 
             <h2 class="header">Home Page</h2>
-            
+
             @if (Auth::check())
-                <?php //echo 'This comment will not be present in the rendered HTML' ?>
-                {{-- This comment will not be present in the rendered HTML --}}
-                
-                <?php echo $name ?> - <?php echo $age ?>
-                <br />
-                {{ $name }} - {{ $age }}
-                <br />
-
-                <?php if (10 > 2) {
-                    echo 'true';
-                } else {
-                    echo 'false';
-                } ?>
-                <br />
-                @if (10 > 2)
-                    true
-                @else
-                    false
-                @endif
-                <br />
-
-                <?php foreach (range(1,5) as $number) {
-                    echo $number;
-                    echo '<br />';
-                } ?>
-
-                @foreach (range(1,5) as $number)
-                    {{ $number }}
-                    <br />
-                @endforeach
+                <p>Only visible for logged in users.</p>
             @endif
 
         </div>
